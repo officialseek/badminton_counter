863b9368bb5b8e2694ea07ae989fff74328179b2
Sebastian Ekström <SebastiaN___@live.se>
2026-02-22 10:58:50 +0100

    Build for deploy to Netlify

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: './',
+  server: {
+    host: true, // Lyssnar på 0.0.0.0 så telefon/andra enheter på samma WiFi når servern
+    port: 5173,
+  },
 })
